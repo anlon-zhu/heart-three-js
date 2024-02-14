8e115c74eee8a187f1221f9f52a1fe733d4409e9
Anlon Zhu <zhu.anlon@gmail.com>
2024-02-14 02:18:44 +0000

    import

--- src/HeartModel.js
+++ src/HeartModel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
